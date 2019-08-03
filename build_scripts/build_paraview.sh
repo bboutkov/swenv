425e75b4bb8e9fa04230af19090769c20dbb4ae3
Boris Boutkov <boris.boutkov@gmail.com>
2019-08-02 21:52:42 -0400

    update and simplify paraview builds

--- build_scripts/build_paraview.sh
+++ build_scripts/build_paraview.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-export PARAVIEW_VERSION=$1
+PARAVIEW_VERSION=ParaView-5.7.0-RC1-MPI-Linux-64bit
 
 echo "Building all paraview for ${UBCESLAB_SYSTEMTYPE:?undefined}"
 
@@ -11,11 +11,9 @@ mkdir -p ${UBCESLAB_SWENV_PREFIX:?undefined}/sourcesdir/paraview
 
 (cd $UBCESLAB_SWENV_PREFIX/sourcesdir/paraview
 
-if [ ! -f paraview-$PARAVIEW_VERSION.tar.gz ]; then
-  wget  --output-document=paraview-$PARAVIEW_VERSION.tar.gz "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.4&type=binary&os=Linux&downloadFile=ParaView-$PARAVIEW_VERSION-Qt5-OpenGL2-MPI-Linux-64bit.tar.gz"
-  #echo "Couldn't find ParaView tarball $UBCESLAB_SWENV_PREFIX/sourcesdir/paraview/paraview-$PARAVIEW_VERSION.tar.gz"
-  #echo "Need to download from http://www.paraview.org and movie it to the path above."
-fi
+ if [ ! -f $PARAVIEW_VERSION.tar.gz ]; then
+     wget  --output-document=$PARAVIEW_VERSION.tar.gz "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.7&type=binary&os=Linux&downloadFile=$PARAVIEW_VERSION.tar.gz"
+ fi
 )
 
 TOPDIR=${UBCESLAB_SWENV_PREFIX:?undefined}/apps/paraview
@@ -25,9 +23,7 @@ rm -rf $PARAVIEW_DIR
 # For the ParaView tar ball, we just need to untar in the right place
 mkdir -p $TOPDIR
 cd $TOPDIR
-tar xvf $UBCESLAB_SWENV_PREFIX/sourcesdir/paraview/paraview-$PARAVIEW_VERSION.tar.gz
-mv ParaView-$PARAVIEW_VERSION-Linux-64bit $PARAVIEW_DIR
-
+tar xvf $UBCESLAB_SWENV_PREFIX/sourcesdir/paraview/$PARAVIEW_VERSION.tar.gz
 
 cd $CURRENT_DIR
 MODULEDIR=$UBCESLAB_SWENV_PREFIX/apps/lmod/modulefiles/paraview
--- build_scripts/build_paraview_version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PARAVIEW_VERSION=4.3.1
-
-./build_paraview.sh $PARAVIEW_VERSION
